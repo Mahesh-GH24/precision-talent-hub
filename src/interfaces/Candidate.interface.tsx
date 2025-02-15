7ba4716e0411fefa9d400c5da969eea6118dfc89
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-02-15 17:18:24 -0600

    Tweaked candidate interface and candidate search - added useEffect to fetch the candidates at the time page load

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,39 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly login: string | null;              //Important
+    readonly id: number | null;
+    readonly node_id: string | null;
+    readonly avatar_url: string | null;         //Important -- for image?
+    readonly gravatar_id: string | null;
+    readonly url: string | null;
+    readonly html_url: string | null;
+    readonly followers_url: string | null;
+    readonly following_url: string | null;
+    readonly gists_url: string | null;
+    readonly starred_url: string | null;
+    readonly subscriptions_url: string | null;
+    readonly organizations_url: string | null;
+    readonly repos_url: string | null;
+    readonly events_url: string | null;
+    readonly received_events_url: string | null;
+    readonly type: string | null;
+    readonly user_view_type: string | null; // second set
+    readonly site_admin: boolean | null;
+    readonly name?: string | null;               //Important
+    readonly company?: string | null;            //Important
+	readonly blog?: string | null;
+	readonly location?: string | null;           //Important
+	readonly email?: string | null;              //Important
+	readonly hireable?: string | null;
+	readonly bio?: string | null;                //Important
+	readonly twitter_username?: string | null;
+	readonly public_repos?: number | null;
+	readonly public_gists?: number | null;
+	readonly followers?: number | null;
+	readonly following?: number | null;
+	readonly created_at?: string | null;
+	readonly updated_at?: string | null;
+}
+
+
+	
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,31 @@
-// import { useState, useEffect } from 'react';
-// import { searchGithub, searchGithubUser } from '../api/API';
+import { useEffect  } from 'react';
+import { searchGithub } from '../api/API';
+// import { searchGithubUser } from '../api/API';
+// import { useState } from 'react';
+
+//import the Candidate Interface
+// import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+
+  useEffect(() => {
+    //Call the list of random Candidates and store in data
+    //load once during page load
+    const data: any = searchGithub();
+    console.log(data);
+  }, []);
+
+  //return <h1>CandidateSearch</h1>;
+  return (
+    <>
+      <section id='searchSection'>
+        <form>
+          <button type='button' id='IgnoreBtn'> - </button>
+          <button type='button' id='AddBtn'> + </button>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default CandidateSearch;
