b84ddbbf2b94f81161c6103bf876c4777eef99b1
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-02-16 18:21:43 -0600

    Removed unnecessary properties from interface and added required functions within CandidateSearch to use 2 apis

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,31 +1,79 @@
-import { useEffect  } from 'react';
-import { searchGithub } from '../api/API';
-// import { searchGithubUser } from '../api/API';
-// import { useState } from 'react';
+//imports
+import { useEffect, useState } from 'react';
+import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
-//import the Candidate Interface
-// import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
 
+  //State variable to add Saved Candidate
+  const [candidate_saved, saveCandidate] = useState<Candidate | null>(null);
+  const [loading, saveLoading] = useState<boolean>(true);
+  const [error, saveError] = useState<string | null>(null);
+
+  //First call to API to fetch a random Candidate via useEffect - once during load and then everytime when state variable changes
   useEffect(() => {
-    //Call the list of random Candidates and store in data
-    //load once during page load
-    const data: any = searchGithub();
-    console.log(data);
+    getRandomCandidate();
   }, []);
 
-  //return <h1>CandidateSearch</h1>;
+  //getRandomCandidate
+  const getRandomCandidate = async ()=> {
+    //Set All good State
+    saveLoading(true);
+    saveError(null);
+    
+    try{
+      //Get Random Candidates via API
+      const candidates = await searchGithub();
+      if (candidates.length > 0) {
+        //pick the 1st candidate
+        const firstCandidate = candidates[0];
+        //Now fetch detailed info about the candidate
+        const infoCandidate = await searchGithubUser(firstCandidate.login);
+        if (infoCandidate) {
+          saveCandidate(infoCandidate);
+        } else {
+          saveError("Error retrieving detailed information about the candidate");
+        }
+      } else {
+        saveError("No more candidates are available");
+        saveCandidate(null);
+      }
+    } catch (err) {
+      saveError("Unknown error while fetching candidate data")
+    } finally {
+      saveLoading(false);
+    }
+  };
+
+  //ignoreCandidate - Simply move on to next Candidate without Saving
+  // const ignoreCandidate =() => {
+  //   getRandomCandidate();
+  // }
+
+  //addCandidate - Save to local storage and move to next Candidate
+  // const addCandidate =() => {
+  //   if (candidate_saved) {
+  //     //Get All Saved Candidates from localstorage
+  //     const savedCandidates = JSON.parse(localStorage.getItem("Saved_Candidates") || "[]");
+  //     savedCandidates.push(candidate_saved);
+  //     localStorage.setItem("Saved_Candidates", JSON.stringify(savedCandidates));
+  //   }
+  //   //Fetch the next Candidate and display
+  //   getRandomCandidate();
+  // };
+
   return (
-    <>
-      <section id='searchSection'>
-        <form>
-          <button type='button' id='IgnoreBtn'> - </button>
-          <button type='button' id='AddBtn'> + </button>
-        </form>
-      </section>
-    </>
-  );
+    <div>
+      <h1>CandidateSearch</h1>
+      {loading && <p> Still Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {candidate_saved && !loading && !error && (
+        <p>Candidate Detailed Information Coming Soon...</p>
+      )}
+      {!candidate_saved && !loading && !error && <p> No Candidates to Display</p>}
+    </div>
+  ); 
 };
 
 export default CandidateSearch;
